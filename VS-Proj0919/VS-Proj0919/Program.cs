e2d33299ece3a7cdc388d5067041b2ca551106b1
yapeng hao <yapeng.hao@email.com>
2019-09-19 14:05:19 -0400

    test

---
+++ VS-Proj0919/VS-Proj0919/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace VS_Proj0919
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("project-0919");
+            Thread.Sleep(3000);
+            
+        }
+    }
+}
